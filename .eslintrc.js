190c7c1612f9cbf80a757e7c169877da7bcb7aad
Shawn Kost <shawnmkost@gmail.com>
2024-03-13 14:17:57 -0700

    chore: create format script and format files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,22 @@ module.exports = {
   env: {
     node: true,
     es2022: true,
-    browser: true,
+    browser: true
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
       },
-      rules: {},
-    },
-  ],
+      rules: {}
+    }
+  ]
 };
