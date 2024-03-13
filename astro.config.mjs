9c50a0e6878b84b211c6d84c9a676863ec6485e4
Shawn Kost <shawnmkost@gmail.com>
2024-03-13 13:57:34 -0700

    chore: add deps and adjust deps configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { SITE } from "./src/config"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE.website
+  integrations: [tailwind(), react(), sitemap()]
+});
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const SITE = {
+  website: 'https://blog.shawnkost.dev', // replace this with your deployed domain
+  author: 'Shawn Kost',
+  desc: 'A blog about web development, programming, and other tech topics.',
+  title: 'Shawn Kost',
+  postPerPage: 5
+};
