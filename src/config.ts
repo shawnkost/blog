7f828fa9db4931314aa96c30b8474a45dfbde2ce
Shawn Kost <shawnmkost@gmail.com>
2024-03-14 10:20:55 -0700

    chore: add missing property to config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,10 @@
 import type { Site } from './types';
 
 export const SITE: Site = {
-  website: 'https://blog.shawnkost.dev', // replace this with your deployed domain
+  website: 'https://blog.shawnkost.dev',
   author: 'Shawn Kost',
   desc: 'A blog about web development, programming, and other tech topics.',
   title: 'Shawn Kost',
+  ogImage: '',
   postPerPage: 5
 };
