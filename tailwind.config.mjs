fc4d48a8b1c834aa6ac5a97aadd651364df41068
Shawn Kost <shawnmkost@gmail.com>
2024-03-13 19:30:50 -0700

    style: adjust tailwind theme and save theme preference

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,19 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        mono: ["IBM Plex Mono", "monospace"],
+      },
+			textColor: {
+				 base: 'rgb(var(--color-text-base))',
+				 accent: 'rgb(var(--color-accent))',
+			},
+			backgroundColor: {
+				base: 'rgb(var(--color-bg-base))',
+				accent: 'rgb(var(--color-accent))',
+			},
+		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 }
